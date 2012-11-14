ceea5f977e497b9bc0c81cbea9e4622cd843b449
Tim Romberg <tim.jakobsson@gmail.com>
2012-11-14 22:06:12 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dondi::Application.config.secret_token = 'a37c6e4614ede9e7bb480bc3e1ed1cffa54d93f987714f80363f4b41c7a32aa86854848761953201abed7dedbda651b1e770c821bcb0eae601424ada9b002c58'
